659c68af6dd1d34ef7455280e35c352c3fd6b64d
KirillDmit <kirilldmitriev20015@gmail.com>
2024-11-04 00:38:40 +0500

    swagger
    + swagger config

--- src/main/java/ru/dmitriev/NauJavaSpring/controller/UserRestController.java
+++ src/main/java/ru/dmitriev/NauJavaSpring/controller/UserRestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import ru.dmitriev.NauJavaSpring.repository.UserRepository;
 import ru.dmitriev.NauJavaSpring.entity.User;
 
@@ -22,7 +24,8 @@ public class UserRestController {
     }
 
     @GetMapping("/findByName")
-    public List<User> getUsersByName(@RequestParam String name) {
+    @Operation(summary = "find users by name", description = "find all users by name")
+    public List<User> getUsersByName(@Parameter(description = "user name") @RequestParam String name) {
         return userRepository.findByName(name);
     }
 }
